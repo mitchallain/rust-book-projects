3a483d85a5560875bc11eb59ca25d83a35978b92
Mitchell Allain <allain.mitch@gmail.com>
2023-10-15 10:58:07 -0500

    Examples up to start of chapter 4

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,9 +5,9 @@ use std::cmp::Ordering;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    // println!("The secret number is: {}", secret_number);
+    // println!("The secret number is: {secret_number}");
 
     loop {
 
@@ -24,7 +24,7 @@ fn main() {
             Err(_) => continue,
         };
 
-        println!("You guessed: {}", guess);
+        println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
